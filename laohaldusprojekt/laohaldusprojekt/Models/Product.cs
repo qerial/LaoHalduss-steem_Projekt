e5a0a5bbc68cf2d3bee921f16a49628cd2165704
qerial <avigolainen@gmail.com>
2026-05-21 18:19:47 +0300

    Added Product

---
+++ laohaldusprojekt/laohaldusprojekt/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace laohaldusprojekt.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string TooteNimi { get; set; }
+        public int Kogus { get; set; }
+        public decimal Hind { get; set; }
+        public string Kategooria { get; set; }
+    }
+}
