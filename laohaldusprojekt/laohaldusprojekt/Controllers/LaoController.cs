a24503857d55133c6212153f9603bd7774ea2c2b
kermoblock <mrdisxo@gmail.com>
2026-05-26 20:13:10 +0300

    IndexHtmlEhitus

--- laohaldusprojekt/laohaldusprojekt/Controllers/LaoController.cs
+++ laohaldusprojekt/laohaldusprojekt/Controllers/LaoController.cs
@@ -1,6 +1,19 @@
-﻿namespace laohaldusprojekt.Controllers
+﻿using laohaldusprojekt.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace laohaldusprojekt.Controllers
 {
     public class LaoController
     {
+
+        private readonly LaohaldusContext _context;
+
+        public LaoController
+            (
+                LaohaldusContext context
+            )
+        {
+            _context = context;
+        }
     }
 }
