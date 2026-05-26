4ae58fa8df0cf50eb764816eb40855948494b801
qerial <avigolainen@gmail.com>
2026-05-26 19:01:30 +0300

    Update to Product + DbInitializer

---
+++ laohaldusprojekt/laohaldusprojekt/Data/DbInitializer.cs
@@ -0,0 +1,27 @@
+﻿using laohaldusprojekt.Models;
+
+namespace laohaldusprojekt.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(LaohaldusContext context)
+        {
+            context.Database.EnsureCreated();
+            if (context.Students.Any())
+            {
+                return;   
+            }
+            var products = new Product[]
+            {
+            new Product { TooteNimi="Lihapihvid, MAKS&MOORITS", Kogus=4, Hind=3.55, Kategooria="Liha- ja kalatooted" },
+            new Product { TooteNimi="Kodune šašlõkk", Kogus=2, Hind=5, Kategooria="Liha- ja kalatooted" },
+            new Product { TooteNimi="Juustupulgad Pik-Nik", Kogus=5, Hind=3.19, Kategooria="Piimatooted" }
+            };
+            foreach (Product s in products)
+            {
+                context.Students.Add(s);
+            }
+            context.SaveChanges();
+        }
+    }
+}
--- laohaldusprojekt/laohaldusprojekt/Models/Product.cs
+++ laohaldusprojekt/laohaldusprojekt/Models/Product.cs
@@ -7,7 +7,7 @@ namespace laohaldusprojekt.Models
         public int Id { get; set; }
         public string TooteNimi { get; set; }
         public int Kogus { get; set; }
-        public decimal Hind { get; set; }
+        public double Hind { get; set; }
         public string Kategooria { get; set; }
     }
 }
