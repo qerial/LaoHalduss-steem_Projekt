6bd15dcb7697ffbf3dfc7659af1f00d65f0aedb1
kermoblock <mrdisxo@gmail.com>
2026-05-27 20:26:16 +0300

    ProductDelete

--- laohaldusprojekt/laohaldusprojekt/Controllers/ProductController.cs
+++ laohaldusprojekt/laohaldusprojekt/Controllers/ProductController.cs
@@ -1,6 +1,8 @@
 using laohaldusprojekt.Data;
+using laohaldusprojekt.Models;
 using laohaldusprojekt.ViewModel;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 
 namespace laohaldusprojekt.Controllers
@@ -53,8 +55,52 @@ namespace laohaldusprojekt.Controllers
             return View(vm);
         }
 
-       
-        
+        public async Task<IActionResult> Delete(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            var model = new ProductDeleteViewModel
+            {
+                Id = product.Id,
+                TooteNimi = product.TooteNimi,
+                Kogus = product.Kogus,
+                Hind = product.Hind,
+                Kategooria = product.Kategooria
+            };
+            return View(model);
+        }
+
+        public async Task<IActionResult> DeletePost(int id)
+        {
+            try
+            {
+                Product delete = new Product()
+                {
+                    Id = id,
+                };
+                //teine variant
+                //var delete = await _context.Students
+                //    .FirstOrDefaultAsync(x => x.Id == id);
+
+                _context.Products.Remove(delete);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (DbUpdateException)
+            {
+                return RedirectToAction(nameof(Delete), new { id = id, saveChangesError = true });
+                throw;
+            }
+
+            return RedirectToAction(nameof(Delete));
+        }
+
+
+
 
     }
 }
